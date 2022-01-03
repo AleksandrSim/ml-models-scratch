7bb593e8a4e208bb317cd602c0719c68f4399235
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-01-03 16:19:39 +0400

    Update simple_two_hidden_layers.py

--- neural_networks/simple_two_hidden_layers.py
+++ neural_networks/simple_two_hidden_layers.py
@@ -27,15 +27,6 @@ def read_data(file_name):
 
 
 
-data   = read_data('train.csv')
-data_val = read_data('valid.csv')
-y=  np.array([i[0] for i in data])
-X = (np.array([i[1] for i in data])/255).astype('float32')
-y_valid = np.array([i[0] for i in data_val])
-X_valid = (np.array([i[1] for i in data_val])/255).astype('float32')
-y = np.array([one_hot_code_encoding(i) for i in y])
-y_valid = np.array([one_hot_code_encoding(i) for i in y_valid])
-
 
 
 
