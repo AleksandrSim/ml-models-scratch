54fa2ba148d08f9bd067491978104dc16e4d68d8
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-01-03 16:05:11 +0400

    Create simple_two_hidden_layers.py

---
+++ neural_networks/simple_two_hidden_layers.py
@@ -0,0 +1,159 @@
+import numpy as np
+import time
+
+
+
+
+def one_hot_code_encoding(num):
+    num = int(num)
+    lis = [0,0,0,0,0,0,0,0,0,0]
+    to_change = list(lis)
+    to_change[num] = to_change[0+num] + 1
+    return to_change
+
+def read_data(file_name):
+   data_set = []
+   with open(file_name, "rt") as f:
+       for line in f:
+           line = line.replace("\n", "")
+           tokens = line.split(",")
+           label = int(tokens[0])
+           attribs = []
+           for i in range(784):
+               attribs.append(float(tokens[i + 1]))
+           data_set.append([label, attribs])
+   return data_set
+
+
+
+
+data   = read_data('train.csv')
+data_val = read_data('valid.csv')
+y=  np.array([i[0] for i in data])
+X = (np.array([i[1] for i in data])/255).astype('float32')
+y_valid = np.array([i[0] for i in data_val])
+X_valid = (np.array([i[1] for i in data_val])/255).astype('float32')
+y = np.array([one_hot_code_encoding(i) for i in y])
+y_valid = np.array([one_hot_code_encoding(i) for i in y_valid])
+
+
+
+
+def sigmoid(self, x, derivative=False):
+    if derivative:
+        return (np.exp(-x))/((np.exp(-x)+1)**2)
+    return 1/(1 + np.exp(-x))
+
+def softmax(self, x):
+    exps = np.exp(x - x.max())
+    return exps / np.sum(exps, axis=0)
+
+class DeepNeuralNetwork():
+    def __init__(self, sizes, epochs=150, l_rate=0.05):
+        self.sizes = sizes
+        self.epochs = epochs
+        self.l_rate = l_rate
+
+        self.params = self.initialization()
+
+    def sigmoid(self, x, derivative=False):
+        if derivative:
+            return (np.exp(-x))/((np.exp(-x)+1)**2)
+        return 1/(1 + np.exp(-x))
+
+    def softmax(self, x, derivative=False):
+        # Numerically stable with large exponentials
+        exps = np.exp(x - x.max())
+        if derivative:
+            return exps / np.sum(exps, axis=0) * (1 - exps / np.sum(exps, axis=0))
+        return exps / np.sum(exps, axis=0)
+
+    def initialization(self):
+        # number of nodes in each layer
+        input_layer=self.sizes[0]
+        hidden_1=self.sizes[1]
+        hidden_2=self.sizes[2]
+        output_layer=self.sizes[3]
+
+        params = {
+            'W1':np.random.randn(hidden_1, input_layer) * np.sqrt(1. / hidden_1),
+            'W2':np.random.randn(hidden_2, hidden_1) * np.sqrt(1. / hidden_2),
+            'W3':np.random.randn(output_layer, hidden_2) * np.sqrt(1. / output_layer)
+        }
+
+        return params
+
+    def forward_pass(self, x_train):
+        params = self.params
+
+        params['A0'] = x_train
+
+        params['Z1'] = np.dot(params["W1"], params['A0'])
+        params['A1'] = self.sigmoid(params['Z1'])
+
+        params['Z2'] = np.dot(params["W2"], params['A1'])
+        params['A2'] = self.sigmoid(params['Z2'])
+
+        params['Z3'] = np.dot(params["W3"], params['A2'])
+        params['A3'] = self.softmax(params['Z3'])
+
+        return params['A3']
+
+    def backward_pass(self, y_train, output):
+
+        params = self.params
+        change_w = {}
+
+        error = 2 * (output - y_train) / output.shape[0] * self.softmax(params['Z3'], derivative=True)
+        change_w['W3'] = np.outer(error, params['A2'])
+
+        error = np.dot(params['W3'].T, error) * self.sigmoid(params['Z2'], derivative=True)
+        change_w['W2'] = np.outer(error, params['A1'])
+
+        error = np.dot(params['W2'].T, error) * self.sigmoid(params['Z1'], derivative=True)
+        change_w['W1'] = np.outer(error, params['A0'])
+
+        return change_w
+
+    def update_network_parameters(self, changes_to_w):
+
+        
+        for key, value in changes_to_w.items():
+            self.params[key] -= self.l_rate * value
+
+    def compute_accuracy(self, x_val, y_val):
+
+        predictions = []
+
+        for x, y in zip(x_val, y_val):
+            output = self.forward_pass(x)
+            pred = np.argmax(output)
+            predictions.append(pred == np.argmax(y))
+        
+        return np.mean(predictions)
+
+    def train(self, x_train, y_train, x_val, y_val):
+        start_time = time.time()
+        for iteration in range(self.epochs):
+            for x,y in zip(x_train, y_train):
+                output = self.forward_pass(x)
+                changes_to_w = self.backward_pass(y, output)
+                self.update_network_parameters(changes_to_w)
+            accuracy = self.compute_accuracy(x_val, y_val)
+            print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2:.2f}%'.format(
+                iteration+1, time.time() - start_time, accuracy * 100))
+            
+            
+
+
+
+
+net = DeepNeuralNetwork(sizes=[784, 128, 64, 10])
+
+
+
+net.train(X,  y, X_valid, y_valid)
+
+
+
+
